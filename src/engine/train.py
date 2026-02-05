9753ef2f9f2709d8b77cddc8ea5d8c4477d9d4f3
toni <nickschtino@gmail.com>
2026-02-05 11:52:52 +0100

    train

--- src/engine/train.py
+++ src/engine/train.py
@@ -9,7 +9,78 @@ def train(
     epochs: int,
     optimizer: optim.Optimizer,
     scheduler: optim.lr_scheduler.LRScheduler,
-):
+    loss_fn: nn.Module,
+    device,
+) -> tuple[list[float], list[float]]:
+
+    train_losses = []
+    val_losses = []
 
     for epoch in range(epochs):
-        pass
+
+        train_loss = train_one_epoch(
+            model=model,
+            train_dataloader=train_dataloader,
+            optimizer=optimizer,
+            scheduler=scheduler,
+            loss_fn=loss_fn,
+            device=device,
+        )
+
+        val_loss = val_one_epoch(model=model, val_dataloader=val_dataloader, loss_fn=loss_fn, device=device)
+
+        train_losses.extend(train_loss)
+        val_losses.extend(val_loss)
+
+        if (epoch + 1) % 1 == 0:
+            print(f"Epoch {epoch+1}/{epochs}:")
+            print(f"Average train loss last epoch: {sum(train_loss) / len(train_loss):.3f}")
+            print(f"Average validation loss last epoch: {sum(val_loss) / len(val_loss):.3f}")
+
+    return train_losses, val_losses
+
+
+def train_one_epoch(
+    model: nn.Module,
+    train_dataloader: DataLoader,
+    optimizer: optim.Optimizer,
+    scheduler: optim.lr_scheduler.LRScheduler,
+    loss_fn: nn.Module,
+    device: str,
+) -> list[float]:
+    train_losses = []
+
+    for X, y in train_dataloader:
+        X = X.to(device)
+        y = y.to(device)
+
+        # forward pass
+        y_pred = model(X)
+
+        # backward pass
+        optimizer.zero_grad()
+        loss = loss_fn(y_pred, y)
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+
+        # track loss
+        train_losses.append(loss.item())
+
+    return train_losses
+
+
+def val_one_epoch(model: nn.Module, val_dataloader: DataLoader, loss_fn: nn.Module, device: str) -> list[float]:
+    val_losses = []
+    for X, y in val_dataloader:
+        X = X.to(device)
+        y = y.to(device)
+
+        # forward pass
+        y_pred = model(X)
+
+        # track loss
+        loss = loss_fn(y_pred, y)
+        val_losses.append(loss.item())
+
+    return val_losses
--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import torch
+from torch import nn
 from torch.utils.data import DataLoader, TensorDataset
 
 from micro_data_utils.micro_dataset import (
@@ -16,10 +17,21 @@ def main() -> None:
 
     # read args
     epochs, batch_size, lr, block_size, hidden_size, embedding_size = read_args()
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(
+        f"Running on the following parameters: \n"
+        f"device: {device}\n"
+        f"epochs: {epochs}\n"
+        f"batch_size: {batch_size}\n"
+        f"lr: {lr}\n"
+        f"block_size: {block_size}\n"
+        f"hidden_size: {hidden_size}\n"
+        f"embedding_size: {embedding_size}\n"
+    )
 
     # initialize micro_data_utils
     full_data = get_micro_dataset()
-
+    full_data = full_data[0 : int(len(full_data) * 0.3)]
     stoi, itos = get_micro_transformer(full_data)
 
     X_train, y_train, X_val, y_val, X_test, y_test = micro_transform_and_split_data(
@@ -41,18 +53,25 @@ def main() -> None:
         block_size=block_size,
         hidden_dims=hidden_size,
     )
+    micro_model = micro_model.to(device)
 
     optimizer = torch.optim.SGD(micro_model.parameters(), lr=lr, momentum=0.9)
     scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
+    loss_fn = nn.CrossEntropyLoss()
 
-    train(
+    train_loss, val_loss = train(
         model=micro_model,
         train_dataloader=train_dataloader,
         val_dataloader=val_dataloader,
         epochs=epochs,
         optimizer=optimizer,
         scheduler=scheduler,
+        loss_fn=loss_fn,
+        device=device,
     )
+
+    print(f"Average training loss: {sum(train_loss) / len(train_loss):.3f}")
+    print(f"Average val loss: {sum(val_loss) / len(val_loss):.3f}")
 
 
 if __name__ == "__main__":
